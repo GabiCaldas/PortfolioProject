ed5780184ab07ffbeca418f1a1eb9c700be5da9c
GabiCaldas <101481323+GabiCaldas@users.noreply.github.com>
2022-06-27 13:22:59 -0400

    Update Nashville Housing Data Cleaning Queries.sql.sql

--- Nashville Housing Data Cleaning Queries.sql.sql
+++ Nashville Housing Data Cleaning Queries.sql.sql
@@ -44,7 +44,7 @@ Where a.PropertyAddress is null
 
 
 
----Breaking out Address into Indivudual Columns (Address, City, State)
+---Breaking out Address into Individual Columns (Address, City, State)
 
 Select *
 From PortfolioProject.dbo.NashvilleHousing
